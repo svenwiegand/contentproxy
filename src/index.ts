427b19a4e14ee649a6c3e9cff0d544ce27da7ab4
Sven Wiegand <sven.wiegand@orgavision.com>
2019-08-25 15:52:48 +0200

    Making basic lambda with query parameters and HTML result work

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+interface RequestEvent {
+    queryStringParameters: {
+        url: string
+    },
+    multiValueQueryStringParameters: {
+        cookie: Array<string> | undefined
+    }
+}
+
 export const handler = async  (event: any = {}): Promise<any> => {
     const response = JSON.stringify(event, null, 2)
     return {
         isBase64Encoded: false,
         statusCode: 200,
-        headers: {},
-        body: response
+        headers: {
+            'Content-Type': 'text/html; charset=UTF-8'
+        },
+        body: '<html><head></head><body>' + event.queryStringParameters.url + '</body></html>'
     }
 }

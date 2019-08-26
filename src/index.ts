59ea63b76462bcd4cc1a447df9b67253d95d5b9a
Sven Wiegand <sven.wiegand@orgavision.com>
2019-08-26 11:29:36 +0200

    Added cookie handling.

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import fetch from 'node-fetch'
 
 interface RequestEvent {
     queryStringParameters: {
-        url: string
-    },
-    multiValueQueryStringParameters: {
-        cookie: Array<string> | undefined
+        url: string,
+        cookie: string | undefined
     }
 }
 
 export const handler = async (event: RequestEvent): Promise<any> => {
     const url = event.queryStringParameters.url
-    const response = await fetch(url)
+    const cookie = event.queryStringParameters.cookie
+    const headers = cookie ? { Cookie: cookie } : undefined
+    const response = await fetch(url, { headers: headers })
     const html = await response.text()
     return {
         isBase64Encoded: false,

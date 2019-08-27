f78fed991131f8a39fc6952d12e61b85e87e67fb
Sven Wiegand <sven.wiegand@orgavision.com>
2019-08-27 17:27:05 +0200

    Added meta data

--- src/index.ts
+++ src/index.ts
@@ -27,11 +27,30 @@ function buildLambdaResult(parseResult: ParseResult): LambdaResult {
         headers: {
             'Content-Type': 'text/html; charset=utf-8'
         },
-        body: buildHtml(parseResult.title, parseResult.content || '')
+        body: buildHtml(parseResult)
     }
 }
 
-function buildHtml(title: string | null, content: string): string {
-    const titleTag = title ? `<title>${title}</title>` : ''
-    return `<!DOCTYPE html>\n<html><head><meta charset="utf-8">${titleTag}</head><body>${content}</body></html>`
+function buildHtml(parseResult: ParseResult): string {
+    const metaTag = (name: string, content: string | null): string =>
+        content ? `<meta name="${name}" content="${content.replace('"', "'")}">` : ''
+
+    const titleTag = parseResult.title ? `<title>${parseResult.title}</title>` : ''
+    const metaAuthor = metaTag('author', parseResult.author)
+    const metaDate = metaTag('date', parseResult.date_published)
+    const metaDescription = metaTag('description', parseResult.excerpt)
+    const metaImage = metaTag('twitter:image', parseResult.lead_image_url)
+
+    return `<!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            ${titleTag}
+            ${metaAuthor}
+            ${metaDate}
+            ${metaDescription}
+            ${metaImage}
+        </head>
+        <body>${parseResult.content}</body>
+        </html>`
 }

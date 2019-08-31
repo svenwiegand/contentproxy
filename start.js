aef0d5e412b1013cd1944b7606c9dc0d2b84453e
Sven Wiegand <sven.wiegand@orgavision.com>
2019-08-31 14:09:34 +0200

    Other default test page.

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 const { handler } = require('./dist/index');
 const event = {
     queryStringParameters: {
-        url: 'https://www.heise.de/select/ct/2019/18/1566917336782380',
+        url: 'https://www.gruene.de/themen/digitalisierung',
         parser: 'mercury'
     }
 };
